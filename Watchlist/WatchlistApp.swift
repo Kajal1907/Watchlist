e725405656280d4b7d282179c3eff8b794cb0b2e
Kajal <kajal.singh@olx.in>
2025-03-31 13:40:24 +0530

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Kajal on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
